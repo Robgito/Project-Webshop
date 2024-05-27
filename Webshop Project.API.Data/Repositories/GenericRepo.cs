2a02df2ae6e8d21dcbc18a7aa4b582f28fdf1965
Robin Boone <90094863+Robgito@users.noreply.github.com>
2024-05-27 10:52:25 +0200

    Generic Repo Added

---
+++ Webshop Project.API.Data/Repositories/GenericRepo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webshop_Project.API.Data.Repositories
+{
+    public class GenericRepo<T> : IGenericRepo<T> where T : class
+    {
+        private WebshopDBContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        public GenericRepo(WebshopDBContext dBContext)
+        {
+            _dbContext = dBContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<T> GetItemByIDAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllItemAsync()
+        {
+            return await _dbSet.ToArrayAsync();
+        }
+
+        public async Task AddItemAsync(T Item)
+        {
+            await _dbSet.AddAsync(Item);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteItemByIDAsync(T Item)
+        {
+            _dbSet.Remove(Item);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateItemAsync(T Item)
+        {
+            _dbSet.Update(Item);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public Task MakeItemInactiveByIDAsync(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Webshop Project.API.Data/Repositories/IGenericRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace Webshop_Project.API.Data.Repositories
+{
+    public interface IGenericRepo<T>
+    {
+        Task AddItemAsync(T Item);
+        Task DeleteItemByIDAsync(int id);
+        Task<IEnumerable<T>> GetAllItemAsync();
+        Task<T> GetItemByIDAsync(int id);
+        Task UpdateItemAsync(T Item);
+    }
+}
