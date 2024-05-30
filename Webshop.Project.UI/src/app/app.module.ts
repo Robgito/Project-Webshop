c21e245f3467cef7513f501e85841cfdcd691075
Abdul-Althyab <abdul.althyab@gmail.com>
2024-05-30 15:58:24 +0200

    Routerlink van navbar

--- Webshop.Project.UI/src/app/app.module.ts
+++ Webshop.Project.UI/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SmartphonesComponent } from './products/smartphones/smartphones/smartphones.component';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { BasketComponent } from './basket/basket.component';
+import { SmartphoneDetailsComponent } from './products/smartphones/smartphone-details/smartphone-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SmartphonesComponent,
     NavbarComponent,
-    BasketComponent
+    BasketComponent,
+    SmartphoneDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Webshop.Project.UI/src/app/products/smartphones/smartphone-details/smartphone-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smartphone-details',
+  templateUrl: './smartphone-details.component.html',
+  styleUrl: './smartphone-details.component.css'
+})
+export class SmartphoneDetailsComponent {
+
+}
