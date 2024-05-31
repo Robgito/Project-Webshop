32e9ed9bcee512d72701b31ce16222826b534554
jlenoor <166016521+jlenoor@users.noreply.github.com>
2024-05-31 14:24:10 +0200

    Jeffrey_filters

--- Webshop.Project.UI/src/app/app-routing.module.ts
+++ Webshop.Project.UI/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SmartphonesComponent } from './products/smartphones/smartphones/smartphones.component';
 import { SmartphoneDetailsComponent } from './products/smartphones/smartphone-details/smartphone-details.component';
+import { BasketComponent } from './basket/basket.component';
 
 const routes: Routes = [
   { path: 'smartphones', component: SmartphonesComponent },
-  { path: 'smartphone-details', component: SmartphoneDetailsComponent },
+  { path: 'smartphones/:smartphoneId', component: SmartphoneDetailsComponent },
+  { path: 'basket', component: BasketComponent}
 ];
 
 @NgModule({
