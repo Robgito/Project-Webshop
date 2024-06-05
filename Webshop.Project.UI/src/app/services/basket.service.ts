b678be5b38242fa58e7c103e08a47411e77f4b50
Robin Boone <90094863+Robgito@users.noreply.github.com>
2024-06-05 17:02:10 +0200

    Removal of basketproducts through API call

--- Webshop.Project.UI/src/app/services/basket.service.ts
+++ Webshop.Project.UI/src/app/services/basket.service.ts
@@ -7,12 +7,25 @@ import { Smartphone } from '../model/smartphone.model';
   providedIn: 'root',
 })
 export class BasketService {
-  private readonly API_URL =
+  private readonly API_URL_PRODUCTS =
     'https://localhost:7129/api/Basket/GetAllProductsInBasket';
+  private readonly API_URL_BASKET = 'https://localhost:7129/api/';
+  private readonly API_DELETE_BASKETPRODUCT =
+    'https://localhost:7129/api/Basket/DeleteBasketProduct';
 
   constructor(private httpClient: HttpClient) {}
 
   GetProductsInBasket(basketID: number): Observable<any> {
-    return this.httpClient.get(this.API_URL + `?basketID=${basketID}`);
+    return this.httpClient.get(this.API_URL_PRODUCTS + `?basketID=${basketID}`);
+  }
+
+  GetFullBasket(basketID: number): Observable<any> {
+    return this.httpClient.get(this.API_URL_BASKET + `Basket?id=${basketID}`);
+  }
+
+  DeleteBasketProduct(basketProductID: number): void {
+    this.httpClient.delete(
+      this.API_DELETE_BASKETPRODUCT + `?id=${basketProductID}`
+    );
   }
 }
